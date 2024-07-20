0544bb5e37e2099a5a8e04f086b8f6ec24e277e3
benjamin <benjamin.pecantet@gmail.com>
2024-07-20 23:35:45 +0200

    ajout animation page home, footer, btn reserver

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
     new Route("/menu", "La Carte", "/pages/menu.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
