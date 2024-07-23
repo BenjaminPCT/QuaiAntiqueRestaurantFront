3baa8bd14f4bff4c65123edb0b33cac0a31e929b
benjamin <benjamin.pecantet@gmail.com>
2024-07-23 17:39:30 +0200

    ajout page allResa

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,7 +8,8 @@ export const allRoutes = [
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/resaTable", "Mes reservations", "/pages/reservations/resaTable.html"),
+    new Route("/resaTable", "Reservation", "/pages/reservations/resaTable.html"),
+    new Route("/allResa", "Les reservations", "/pages/reservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
