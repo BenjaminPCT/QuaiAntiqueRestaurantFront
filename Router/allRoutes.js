068396f12466f1318f76c198900c876c118d0ec2
benjamin <benjamin.pecantet@gmail.com>
2024-07-23 11:57:35 +0200

    ajout de modale pour account + style signin, signup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
     new Route("/menu", "La Carte", "/pages/menu.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
