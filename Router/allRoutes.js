04b6970954b546a26a21a5365f0e9ea81dc50b5d
benjamin <benjamin.pecantet@gmail.com>
2024-07-24 17:37:24 +0200

    ajout token en dur pour connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
     new Route("/menu", "La Carte", "/pages/menu.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", "js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", "js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/resaTable", "Reservation", "/pages/reservations/resaTable.html"),
---
+++ js/auth/signin.js
@@ -0,0 +1,25 @@
+const EmailInput = document.getElementById("EmailInput");
+const PasswordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials() {
+    // infos factices donc appeler l'api pour verifier les credentials en BDD//
+
+    if(EmailInput.value == "test@mail.com" && PasswordInput.value == "123"){
+        
+
+        // il faudra recuperer le vrai token de l'utilisateur connecté//
+        const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+        setToken(token);
+
+        //placer ce yoken en cookie //
+
+        window.location.replace("/");
+    }
+    else{
+        EmailInput.classList.add("is-invalid");
+        PasswordInput.classList.add("is-invalid");
+    }
+}
