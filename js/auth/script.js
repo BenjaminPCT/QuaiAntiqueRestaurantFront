c74d87791f33ac26b3e9bb30d6c1170f12887009
benjamin <benjamin.pecantet@gmail.com>
2024-07-24 18:16:04 +0200

    ajout de la fonction deconnexion

--- js/auth/script.js
+++ js/auth/script.js
@@ -1,6 +1,11 @@
-
-
 const tokenCookieName = "accesstoken";
+const signoutBtn = document.getElementById("signout-btn");
+
+signoutBtn.addEventListener("click", signout);
+function signout(){
+    eraseCookie(tokenCookieName);
+    window.location.replace("/");  // rediriger vers la page d'accueil
+}
 
 function setToken(token) {
     setCookie(tokenCookieName, token, 7);
