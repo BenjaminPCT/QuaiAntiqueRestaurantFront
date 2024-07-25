f7edae088abd868afe1ad2d5ff0aabab48a25ed0
benjamin <benjamin.pecantet@gmail.com>
2024-07-25 10:32:45 +0200

    ajout logique de gestion des roles sur router.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La Galerie", "/pages/galerie.html"),
-    new Route("/menu", "La Carte", "/pages/menu.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", "js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", "js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/resaTable", "Reservation", "/pages/reservations/resaTable.html"),
-    new Route("/allResa", "Les reservations", "/pages/reservations/allResa.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/galerie", "La Galerie", "/pages/galerie.html", []),
+    new Route("/menu", "La Carte", "/pages/menu.html", []),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"] ,"js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"] ,"js/auth/signup.js"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
+    new Route("/resaTable", "Reservation", "/pages/reservations/resaTable.html", ["client"]),
+    new Route("/allResa", "Les reservations", "/pages/reservations/allResa.html", ["client"]),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
