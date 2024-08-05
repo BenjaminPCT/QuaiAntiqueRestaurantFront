e56133a89fc4011fe1ca48ee92613d89820ff3d7
benjamin <benjamin.pecantet@gmail.com>
2024-08-05 11:56:47 +0200

    coorectif 2 eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import { defineConfig } from 'eslint';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default defineConfig({
+  languageOptions: {
+    globals: globals.browser,
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+  },
+  plugins: {
+    // Vous pouvez ajouter d'autres plugins ici si nécessaire
+  },
+  extends: [
+    'eslint:recommended',
+    pluginJs.configs.recommended,
+  ],
+  rules: {
+    // Ajoutez des règles personnalisées ici si nécessaire
+  },
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    },
+  ],
+});
