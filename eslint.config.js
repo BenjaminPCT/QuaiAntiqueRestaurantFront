e49433c4a167221d43a8c2426d1aef9293510c5c
benjamin <benjamin.pecantet@gmail.com>
2024-08-05 12:14:39 +0200

    correctif eslint 6

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,37 @@
-const globals = require('globals');
-const pluginJs = require('@eslint/js');
+// eslint.config.js
+import { FlatConfigProvider, createFlatConfig } from 'eslint';
+import { recommended as recommendedJsConfig } from '@eslint/js';
 
-module.exports = {
-  languageOptions: {
-    globals: globals.browser,
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
+const config = createFlatConfig([
+  {
+    languageOptions: {
+      globals: {
+        browser: 'readonly',
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
     },
-  },
-  extends: [
-    'eslint:recommended',
-    pluginJs.configs.recommended,
-  ],
-  rules: {
-    // Ajoutez des règles personnalisées ici si nécessaire
-  },
-  overrides: [
-    {
-      files: ['**/*.ts'],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
-      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    plugins: {
+      // Ajoutez d'autres plugins ici si nécessaire
     },
-  ],
-};
+    rules: {
+      // Ajoutez des règles personnalisées ici si nécessaire
+    },
+    // Ajoutez la configuration recommandée de ESLint directement
+    overrides: [
+      {
+        files: ['**/*.ts'],
+        parser: '@typescript-eslint/parser',
+        plugins: ['@typescript-eslint'],
+        rules: {
+          // Règles spécifiques pour TypeScript
+        },
+      },
+    ],
+  },
+  recommendedJsConfig,
+]);
+
+export default config;
