21a508150a81b188ccbd346a9fc28d115e849734
benjamin <benjamin.pecantet@gmail.com>
2024-08-05 12:20:29 +0200

    correctif eslint 8

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,27 @@
 // eslint.config.js
-import { FlatConfigProvider, createFlatConfig } from 'eslint';
-import recommendedJs from '@eslint/js';
-
-const config = createFlatConfig([
-  {
-    languageOptions: {
-      globals: {
-        browser: 'readonly',
-      },
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      // Ajoutez d'autres plugins ici si nécessaire
-    },
-    rules: {
-      // Ajoutez des règles personnalisées ici si nécessaire
-    },
-    overrides: [
-      {
-        files: ['**/*.ts'],
-        parser: '@typescript-eslint/parser',
-        plugins: ['@typescript-eslint'],
-        rules: {
-          // Règles spécifiques pour TypeScript
-        },
-      },
-    ],
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
   },
-  recommendedJs.configs.recommended,
-]);
-
-export default config;
+  extends: [
+    'eslint:recommended',
+    '@eslint/js',
+  ],
+  rules: {
+    // Ajoutez des règles personnalisées ici si nécessaire
+  },
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+    },
+  ],
+};
 
